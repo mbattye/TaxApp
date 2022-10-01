ac18bb67cd4dd0aea5c693eef148763c07b2eef6
Michael Battye <michaelbattye@Michaels-MacBook-Pro.local>
2022-10-01 10:20:29 +0100

    Additions to Your Tax and tabs

--- pages/1_Your_Tax.py
+++ pages/1_Your_Tax.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
 from tools.tax_calculations import Inflation, TaxBrackets
-
 
 st.title("Your Tax")
 
@@ -10,52 +8,51 @@ income = st.number_input("Enter your salary (£):")
 savings = st.number_input("Enter your savings (£):")
 spending = st.number_input("Enter the amount your spend on VATable goods (£):")
 
-st.markdown("## Income Tax")
-st.markdown("Calculate your government tithe")
+tab1, tab2, tab3, tab4 = st.tabs(["Income", "National Insurance", "VAT", "Inflation"])
+
+tab1.markdown("## Income Tax")
+tab1.markdown("Calculate your government tithe")
 
 if income != 0:
     tb = TaxBrackets(income)
     percent_inc, tax_inc = tb.percentage_tax('Income')
-    st.write(f"Your income tax is £{tax_inc} per annum.")
-    st.write(f"Your percentage income tax is {percent_inc:.1%}.")
+    tab1.write(f"Your income tax is £{tax_inc} per annum.")
+    tab1.write(f"Your percentage income tax is {percent_inc:.1%}.")
 
-st.markdown("---")
-st.markdown("## National Insurance Tax")
-st.markdown("Calculate your entitlement tithe")
+tab2.markdown("## National Insurance Tax")
+tab2.markdown("Calculate your entitlement tithe")
 
 if income != 0:
     tb = TaxBrackets(income)
     percent_nat, tax_nat = tb.percentage_tax('National Insurance')
-    st.write(f"Your national insurance tax is £{tax_nat} per annum.")
-    st.write(f"Your percentage national insurance tax is {percent_nat:.1%}.")
+    tab2.write(f"Your national insurance tax is £{tax_nat} per annum.")
+    tab2.write(f"Your percentage national insurance tax is {percent_nat:.1%}.")
 
-st.markdown("---")
-st.markdown("## Inflation")
-st.markdown("Calculate your currency expansion tithe")
+tab3.markdown("## Value Added Tax")
+tab3.markdown("Calculate your trade tithe")
+
+if spending != 0:
+    tb = TaxBrackets(spending)
+    percent_vat, tax_vat = tb.percentage_tax('VAT')
+    tab3.write(f"Your VAT tax is £{tax_vat} per annum.")
+    tab3.write(f"Your percentage VAT tax is {percent_vat:.1%}.")
+
+tab4.markdown("## Inflation")
+tab4.markdown("Calculate your currency expansion tithe")
 
 trailing_months = 12
 
 if income != 0:
     inf_inc = Inflation(income)
     value_inc = inf_inc.value_decrease(trailing_months)
-    st.write(f"Over the last year your income has decreased to £{value_inc} per annum.")
+    tab4.write(f"Over the last year your income has decreased to £{value_inc} per annum.")
 
 if savings != 0:
     inf_sav = Inflation(savings)
     value_sav = inf_sav.value_decrease(trailing_months)
-    st.write(f"Over the last year your savings have decreased to £{value_sav}.")
+    tab4.write(f"Over the last year your savings have decreased to £{value_sav}.")
 
 if income != 0 and savings !=0:
     # Fix this to work with OR (don't need percentage calc with values)
     percent_inf = 1 - (value_inc + value_sav)/(income + savings)
-    st.write(f"Your percentage loss due to inflation is {percent_inf:.1%}.")
-
-st.markdown("---")
-st.markdown("## Value Added Tax")
-st.markdown("Calculate your trade tithe")
-
-if spending != 0:
-    tb = TaxBrackets(spending)
-    percent_vat, tax_vat = tb.percentage_tax('VAT')
-    st.write(f"Your VAT tax is £{tax_vat} per annum.")
-    st.write(f"Your percentage VAT tax is {percent_vat:.1%}.")
+    tab4.write(f"Your percentage loss due to inflation is {percent_inf:.1%}.")
--- pages/2_UK_Tax.py
+++ pages/2_UK_Tax.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tools.tax_calculations import TaxBrackets
 
 
-
+# https://www.gov.uk/browse/tax
